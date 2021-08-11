9ffae7b53fe1fd8d5a21e39c260a9d8397117fff
Laurane <laurane.mh@gmail.com>
2021-08-11 18:24:07 +0200

    Add route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::resource('materiels', \App\Http\Controllers\MaterielController::class);
+
+Route::resource('salles', \App\Http\Controllers\SalleController::class);
+
+Route::resource('personnes', \App\Http\Controllers\PersonneController::class);
+
+Route::resource('materiel-rooms', \App\Http\Controllers\MaterielSalleController::class);
+
+Route::resource('materiel-people', \App\Http\Controllers\MaterielPersonneController::class);
+
+Route::resource('downgradeds', \App\Http\Controllers\DeclasseeController::class);
+
