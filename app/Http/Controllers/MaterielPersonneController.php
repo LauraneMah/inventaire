8eeca4f96cd827de117ae18b5707d64bf0ffc8c8
Laurane <laurane.mh@gmail.com>
2021-08-16 21:09:09 +0200

    Comment Authentification page Access

--- app/Http/Controllers/MaterielPersonneController.php
+++ app/Http/Controllers/MaterielPersonneController.php
@@ -12,10 +12,10 @@ use Illuminate\Http\Request;
 class MaterielPersonneController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
 
     /**
      * Display a listing of the resource.
